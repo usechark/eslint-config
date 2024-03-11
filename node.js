f767790a61b10917c28f97b30f99fd1d35f1bbbf
Guilherme Minozzi <minozzi60@gmail.com>
2024-03-11 17:17:30 -0300

    feat: add nodejs eslint rules

---
+++ node.js
@@ -0,0 +1,86 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true,
+  },
+  extends: ["airbnb-base", "prettier"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        singleQuote: true,
+        printWidth: 120,
+      },
+    ],
+    "no-empty": [
+      "error",
+      {
+        allowEmptyCatch: true,
+      },
+    ],
+    "no-useless-constructor": "off",
+    "no-empty-function": [
+      "error",
+      {
+        allow: ["constructors", "methods", "asyncMethods"],
+      },
+    ],
+    "no-console": [
+      "warn",
+      {
+        allow: ["info", "warn", "error"],
+      },
+    ],
+    "max-len": "off",
+    "import/extensions": [
+      "error",
+      "ignorePackages",
+      {
+        ts: "never",
+        tsx: "never",
+      },
+    ],
+    "import/order": [
+      "error",
+      {
+        groups: [
+          "builtin",
+          "external",
+          "internal",
+          "parent",
+          "sibling",
+          "index",
+          "type",
+          "object",
+        ],
+        alphabetize: {
+          order: "asc",
+          caseInsensitive: true,
+        },
+        "newlines-between": "always",
+      },
+    ],
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        selector: "interface",
+        format: ["PascalCase"],
+        custom: {
+          regex: "^I[A-Z]",
+          match: true,
+        },
+      },
+    ],
+  },
+};
